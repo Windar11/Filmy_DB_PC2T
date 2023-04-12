b3da809219e912b8de7ac606c635bd1c08e72590
Otas02CZ <koci.ota02@gmail.com>
2023-04-12 23:56:02 +0200

    kleine update

--- src/FilmDB.java
+++ src/FilmDB.java
@@ -3,8 +3,22 @@ import java.util.ArrayList;
 public class FilmDB {
     ArrayList<Film> films = new ArrayList<>();
 
+    public void addFilm(Film film) {
+        this.films.add(film);
+    }
 
-    public void getFilmsByName() {
 
+
+    public void removeFilm(Film film) {
+        films.remove(film);
+    }
+
+    public ArrayList<Film> getFilmsByName(String filmName) {
+        ArrayList<Film> foundFilms = new ArrayList<>();
+        for (Film film: films) {
+            if (film.getName().equals(filmName))
+                foundFilms.add(film);
+        }
+        return foundFilms;
     }
 }
