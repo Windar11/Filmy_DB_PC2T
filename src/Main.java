f5b0d56554fc9e6a6e9ff26f6355fbb48e5d0750
Otas02CZ <koci.ota02@gmail.com>
2023-04-30 23:44:49 +0200

    remove debug

--- src/Main.java
+++ src/Main.java
@@ -600,11 +600,6 @@ public class Main {
                         } catch (FilmImportError e) {
                             throw new RuntimeException(e);
                         }
-                        System.out.println("Načítané informácie o filme: ");
-                        System.out.println("Názov filmu: "+film.getName());
-                        System.out.println("Rok vydania: "+film.getReleaseYear());
-                        System.out.println("Typ filmu: "+film.getFilmType());
-                        System.out.println("Hodnotenie filmu: "+film.getDirector().getName());
                         break;
 
                     default:
@@ -620,35 +615,8 @@ public class Main {
 
 
         }while(choice != 11);
-
-        // nasledujici try-catch kod je pouze testovaci, zatim zde asi ponechat, kdyby bylo potreba testovat funkci backendu ci databaze,
-        // zatim ponechej a ignoruj
-        /*try {
-            Film one = databaseBackend.addFilm("name", (short)2002, FilmType.ACTED_FILM, (byte)16);
-            Film two = databaseBackend.addFilm("Titanic", (short)2016, FilmType.ACTED_FILM, (byte)100);
-            databaseBackend.filmAddReview(one, (byte)5, "Dobrej filmeček");
-            CrewMember herec = databaseBackend.filmAddCrewMemberNewCrewMember(two, "holaholahejhej");
-            databaseBackend.filmUpdateDirectorNewDirector(one, "Borec rejža");
-            databaseBackend.filmUpdateDirectorNewDirector(two, "dobrej rejža");
-            databaseBackend.filmAddCrewMemberExistingCrewMember(one, herec);
-            databaseBackend.filmUpdateDirectorExistingDirector(one, herec);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error se holt nastal");
-        }*/
-
-        // pokud vse bude fungovat spravne tak kdyz to prvne otevres a zkusis odtud spustit tak by se to melo bez chyb zapnout a vypnout.
-        // teoreticky muzes pridat do idea data source filmDB.db a nad nim zkusit dat nejaky sql dotaz na Film/Crew/Review a melo by ti to neco vyhodit
-        // pokud nefunguje databaze tak zkus jit do File->Project Structure->Libraries->Pridat->Vyber knihovnu sqlite-jdbc ze slozky libs v koreni projektu a potvrdit
-        // Where is our user interface?
-        // Gone reduced to attoms.
-        // Then we should travel in time before the snap to save it.
-        // We are students. We can pretend we can program all day, but...that up there, that's the jáva...we'll lose.
-
         if (!databaseBackend.saveDataToSQL()) {
             System.out.println("Error pri ukladanie");
-            // vraci FALSE pokud ukladani selze
         }
     }
 
